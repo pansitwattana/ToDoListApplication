987fab594911295a39299e052c6bc3e1c1a309e8
Pansit Wattana <pansitwattana@pansits-mac.local>
2017-02-01 02:37:23 -0800

    Upload

--- To-Do List Tab/FirstViewController.swift
+++ To-Do List Tab/FirstViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var toDoList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let loadToDoList = UserDefaults.standard.object(forKey: "todolist")
+        
+        if let array = loadToDoList as? NSArray {
+            toDoList = array as! [String]
+        
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoList.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
 
-
+        cell.textLabel?.text = toDoList[indexPath.row]
+        
+        return cell
+    }
+    
+    public func AddToDo(toDo :String) -> Void {
+        toDoList.append(toDo)
+        UserDefaults.standard.set(toDoList, forKey: "todolist")
+    }
 }
 
--- To-Do List Tab/SecondViewController.swift
+++ To-Do List Tab/SecondViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var inputText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,28 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let todo = inputText.text
+        
+        if segue.identifier == "onButtonConfirm" {
+            if let destination = segue.destination as? FirstViewController {
+                destination.AddToDo(toDo: todo!)
+            }
+        }
+    }
 
+    @IBAction func onConfirm(_ sender: Any) {
+        let todo = inputText.text
+        
+        let toDoList = UserDefaults.standard.object(forKey: "todolist")
+        
+        var newToDoList = [String]()
+        
+        if let list = toDoList as? NSArray {
+            newToDoList = list as! [String]
+            newToDoList.append(todo!)
+        }
+        
+    }
 }
 
